860712529ce7b2031fc086e9419c38e49bcf876a
Dimitrios Vasilas <dv.vasilas@gmail.com>
2020-11-19 14:52:35 +0100

    importToSheet: more compact sheet structure

--- lobsters/scripts/importToSheet.sh
+++ lobsters/scripts/importToSheet.sh
@@ -7,23 +7,28 @@ set -ex
 
 PROTEUS_DIR=$HOME/go/proteus
 BENCH_DIR=$HOME/proteus-evaluation/lobsters
+CREDENTIAL_FILE=$1
 SHEET_ID=$2
 ROW=$3
 RESULT_DIR=$4
+HEAD_F=$5
 
-export CREDENTIAL_FILE=$1
+export CREDENTIAL_FILE
 export SPREADSHEET_ID=14LnCB-ToJ6hduF-yDpEbeRkWF-M2ywnakdEqig5Tv78
 export SHEET_ID=$SHEET_ID
 
-for filename in "$RESULT_DIR"/*
-do
   echo $filename
   ./utils/format-and-import.py -r $ROW --desc template-config
+  ROW=$((ROW+1))
+
+  utils/format-and-import.py -r $ROW template-config $HEAD_F
   ROW=$((ROW+1))
 
   ./utils/format-and-import.py -r $ROW --desc template-run
   ROW=$((ROW+1))
 
+for filename in "$RESULT_DIR"/*
+do
   ./utils/format-and-import.py -r $ROW template-run $filename
   ROW=$((ROW+1))
 done
