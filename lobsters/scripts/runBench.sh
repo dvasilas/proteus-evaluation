6bd0a54e73d6fe01dec3f42a5b4fbdccab94bdf3
Dimitrios Vasilas <dv.vasilas@gmail.com>
2020-12-03 17:48:16 +0100

    updates to benchmark script

--- lobsters/scripts/runBench.sh
+++ lobsters/scripts/runBench.sh
@@ -7,11 +7,12 @@ set -ex
 
 tag_datastore=$1
 tag_qpu=$2
-system=$3
-deployment=$4
+tag_bench=$3
+system=$4
+deployment=$5
 
-if [ "$#" -ne 4 ] ; then
-    echo "Expecting 3 arguments"
+if [ "$#" -ne 5 ] ; then
+    echo "Expecting 5 arguments"
     exit 1
     fi
 
@@ -24,7 +25,7 @@ deployment_dir=$eval_dir/deployments
 
 docker stack rm qpu-graph
 docker stack rm datastore-proteus
-docker stack rm datastore-mv
+docker stack rm mysql || true 
 docker stop join || true
 docker stop sum || true
 docker stop dsdriver || true
@@ -34,7 +35,7 @@ docker stop bench || true
 if [ $system == "mysql" ] ; then
 	bench_conf=$bench_conf_dir/config-mysql.toml
   	deploymentConf=$deployment_dir/datastore-mv.yml
-	datastore_stack=datastore-mv
+	datastore_stack=mysql
 elif [ $system == "proteus" ] ; then
 	bench_conf=$bench_conf_dir/config-proteus.toml
   	deploymentConf=$deployment_dir/datastore-proteus.yml
@@ -62,10 +63,17 @@ for ((i = 0; i < ${#runParams[@]}; i++)) ; do
 	echo "Proteus image tag : $tag_qpu" >> /tmp/$logfile
 
 	if [ $deployment == "swarm" ] ; then
-		env TAG_DATASTORE=$tag_datastore docker stack deploy --compose-file $deploymentConf $datastore_stack
+		env TAG_DATASTORE=$tag_datastore env TAG_BENCH=$tag_bench docker stack deploy --compose-file $deploymentConf $datastore_stack
 		wait_services_running
 	elif [ $deployment == "local" ] ; then
-		env TAG_DATASTORE=$tag_datastore ./datastore.sh
+		if [ $system == "mysql" ] ; then
+			env TAG_DATASTORE=$tag_datastore ./datastore-mysql.sh
+		elif [ $system == "proteus" ] ; then
+			env TAG_DATASTORE=$tag_datastore ./datastore.sh
+		else
+			echo "Expecting either 'proteus' or 'mysql' as 'system' argument"
+			exit 1
+		fi
 	else
 		echo "Expecting either 'swarm' or 'local' as 'deployment' argument"
 		exit 1
@@ -88,29 +96,27 @@ for ((i = 0; i < ${#runParams[@]}; i++)) ; do
 	sleep 10
 	fi
 
-	if [ $deployment == "swarm" ] ; then
-		$bench -c $bench_conf -l $1 --fr $2 --fw $3 -t $4 >> /tmp/$logfile
-	elif [ $deployment == "local" ] ; then
-		docker run -d --rm --name bench --network test_net --cap-add=NET_ADMIN --volume $HOME/go/proteus-lobsters-bench/config/config-proteus.toml:/config/config.toml --volume $HOME/volume/delay.sh:/delay.sh --volume /tmp:/out bench
+#		$bench -c $bench_conf -l $1 --fr $2 --fw $3 -t $4 >> /tmp/$logfile
+#		docker run -d --rm --name bench --network test_net --cap-add=NET_ADMIN --volume $HOME/go/proteus-lobsters-bench/config/config-proteus.toml:/config/config.toml --volume $HOME/volume/delay.sh:/delay.sh --volume /tmp:/out 127.0.0.1:5000/lobsters-bench:$tag_bench
+
+#	docker run -d --rm --name bench --network test_net --cap-add=NET_ADMIN --volume $HOME/go/proteus-lobsters-bench/config/config-mysql.toml:/config/config.toml --volume $HOME/volume/delay.sh:/delay.sh --volume /tmp:/out 127.0.0.1:5000/lobsters-bench:$tag_bench
 		sleep 10
-		./simulate-latency.sh bench datastore
-		./simulate-latency.sh sum join
-		sleep 10
-		docker exec bench /app/bench/bin/benchmark -c /config/config.toml -l $1 --fr $2 --fw $3 -t $4 > /tmp/$logfile
-	else
-		echo "Expecting either 'swarm' or 'local' as 'deployment' argument"
-		exit 1
-	fi
+#		./simulate-latency.sh bench datastore
+#		./simulate-latency.sh sum join
+#		sleep 10
+#		docker exec bench /app/bench/bin/benchmark -c /config/config.toml -l $1 --fr $2 --fw $3 -t $4 > /tmp/$logfile
+		./utils/service-exec.sh -s mysql_bench -- /app/bench/bin/benchmark -c /config/config.toml -l $1 --fr $2 --fw $3 -t $4 > /tmp/$logfile
 
 	curl -u $NUAGE_LIP6_U:$NUAGE_LIP6_P -T /tmp/$logfile https://nuage.lip6.fr/remote.php/dav/files/$NUAGE_LIP6_U/proteus_bench_logs/$logfile
 		
 	if [ $deployment == "swarm" ] ; then
 		docker stack rm qpu-graph
 	elif [ $deployment == "local" ] ; then
-		docker stop bench
-		docker stop join
-		docker stop sum 
-		docker stop dsdriver
+		docker stop join || true
+		docker stop sum || true
+		docker stop dsdriver || true
+		docker stop datastore || true
+		docker stop bench || true
 	else
 		echo "Expecting either 'swarm' or 'local' as 'deployment' argument"
 		exit 1
