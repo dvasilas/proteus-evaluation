98b1e87cea88a295e654eebfb721e99063884f93
Dimitrios Vasilas <dv.vasilas@gmail.com>
2021-01-31 22:19:20 +0100

    update to proteus/ycsb deployment

--- ycsb/scripts/parse.py
+++ ycsb/scripts/parse.py
@@ -6,9 +6,16 @@ def main():
     with open(sys.argv[1], "r") as fr:
         with open(sys.argv[2], "w") as fw:
             queryMeasurements = {}
+            updateMeasurements = {}
             line = fr.readline()
+            threads = False
             while line:
                 if "threadcount" in line:
+                    line = line.split("=")
+                    line = line[1].strip()[1:-1]
+                    fw.write("Threadcount: " + line + "\n")
+                if not threads and "threads" in line:
+                    threads = True
                     line = line.split("=")
                     line = line[1].strip()[1:-1]
                     fw.write("Threadcount: " + line + "\n")
@@ -16,6 +23,8 @@ def main():
                     line = line.strip().split(", ")
                 if line[0] == "[QUERY]":
                     queryMeasurements[line[1]] = line[2]
+                if line[0] == "[UPDATE]":
+                    updateMeasurements[line[1]] = line[2]
             
                 line = fr.readline()
 
@@ -25,6 +34,14 @@ def main():
             fw.write("[QUERY] 90thPercentileLatency: " + str(float(queryMeasurements["90thPercentileLatency(us)"]) / 1000) + "\n")
             fw.write("[QUERY] 95thPercentileLatency: " + str(float(queryMeasurements["95thPercentileLatency(us)"]) / 1000) + "\n")
             fw.write("[QUERY] 99thPercentileLatency: " + str(float(queryMeasurements["99thPercentileLatency(us)"]) / 1000) + "\n")
+
+
+            fw.write("[UPDATE] Throughput: " + updateMeasurements["Throughput(ops/sec)"] + "\n")
+            fw.write("[UPDATE] AverageLatency: " + str(float(updateMeasurements["AverageLatency(us)"]) / 1000) + "\n")
+            fw.write("[UPDATE] 50thPercentileLatency: " + str(float(updateMeasurements["50thPercentileLatency(us)"]) / 1000) + "\n")
+            fw.write("[UPDATE] 90thPercentileLatency: " + str(float(updateMeasurements["90thPercentileLatency(us)"]) / 1000) + "\n")
+            fw.write("[UPDATE] 95thPercentileLatency: " + str(float(updateMeasurements["95thPercentileLatency(us)"]) / 1000) + "\n")
+            fw.write("[UPDATE] 99thPercentileLatency: " + str(float(updateMeasurements["99thPercentileLatency(us)"]) / 1000) + "\n")
 
 if __name__ == "__main__":
     main()
--- ycsb/scripts/runBench.sh
+++ ycsb/scripts/runBench.sh
@@ -12,7 +12,7 @@ proteus_dir=$HOME/go/proteus
 eval_dir=$HOME/proteus-evaluation/ycsb
 deployment_dir=$eval_dir/deployments
 
-storageStack=cloudserver
+storageStack=storage
 queryengineStack=proteus
 benchStack=ycsb
 
@@ -21,7 +21,9 @@ docker stack rm $queryengineStack || true
 docker stack rm $storageStack || true
 
 storageDeploymentConf=$deployment_dir/storage.yml
-queryengineDeploymentConf=$deployment_dir/partitioned.yml
+#queryengineDeploymentConf=$deployment_dir/partitioned.yml
+#queryengineDeploymentConf=$deployment_dir/global.yml
+queryengineDeploymentConf=$deployment_dir/globalcache.yml
 benchDeploymentConf=$deployment_dir/ycsb.yml
 
 sync
@@ -33,38 +35,56 @@ for ((i = 0; i < ${#runParams[@]}; i++)) ; do
 
 	logfile=$(timestamp_filename)
 
-cp -r ${HOME}/dataset/data0 ${HOME}/dataset/data0_run
-cp -r ${HOME}/dataset/metadata0 ${HOME}/dataset/metadata0_run
-
-ssh -tt small18 cp -r ~/volume/datastore/dataset/ycsb/data1 ~/dataset/data1_run
-ssh -tt small18 cp -r ~/volume/datastore/dataset/ycsb/metadata1 ~/dataset/metadata1_run
-
-ssh -tt small21 cp -r ~/volume/datastore/dataset/ycsb/data2 ~/dataset/data2_run
-ssh -tt small21 cp -r ~/volume/datastore/dataset/ycsb/metadata2 ~/dataset/metadata2_run
-
 docker stack deploy --compose-file $storageDeploymentConf $storageStack
-wait_services_running
+wait_service_running storage_mongo0
+wait_service_running storage_mongo1
+wait_service_running storage_mongo2
 sleep 10
 
-$proteus_dir/mongo/load/load
+#$proteus_dir/mongo/load/load
 
 env TAG_QPU=$tag_qpu docker stack deploy --compose-file $queryengineDeploymentConf $queryengineStack
-wait_services_running
-sleep 10
+wait_service_running proteus_dsdriver-0
+wait_service_running proteus_dsdriver-1
+wait_service_running proteus_dsdriver-2
+wait_service_running proteus_index-0
+wait_service_running proteus_index-1
+wait_service_running proteus_index-2
+wait_service_running proteus_router-0
+wait_service_running proteus_router-1
+wait_service_running proteus_router-2
 
-$proteus_dir/bench/bench $1 > /tmp/$logfile.out
+#$proteus_dir/bench/bench $1 > /tmp/$logfile.out
 
-#env TAG_YCSB=$tag_bench env LOGFILE=$logfile env THREADS=$1 docker stack deploy --compose-file $benchDeploymentConf $benchStack
-#sleep 60
-#wait_service_complete ycsb_ycsb
+env TAG_YCSB=$tag_bench env OUTPUT_FILE_NAME=$logfile env THREADS=$1 env QUERY_PROP=0.95 env UPDATE_PROP=0.05 docker stack deploy --compose-file $benchDeploymentConf $benchStack
 
-#$eval_dir/scripts/parse.py /tmp/$logfile.txt /tmp/$logfile.out
+sleep 60
+wait_service_complete ycsb_ycsb-0
+wait_service_complete ycsb_ycsb-1
+wait_service_complete ycsb_ycsb-2
 
-curl -u $NUAGE_LIP6_U:$NUAGE_LIP6_P -T /tmp/$logfile.out https://nuage.lip6.fr/remote.php/dav/files/$NUAGE_LIP6_U/proteus_bench_logs/$logfile.txt
-#curl -u $NUAGE_LIP6_U:$NUAGE_LIP6_P -T /tmp/$logfile.txt https://nuage.lip6.fr/remote.php/dav/files/$NUAGE_LIP6_U/proteus_bench_logs/$logfile.raw.txt
+rsync small18:/tmp/QUERY_$logfile.1.hdr ${HOME}/res1
+rsync small18:/tmp/UPDATE_$logfile.1.hdr ${HOME}/res1
+rsync small21:/tmp/QUERY_$logfile.2.hdr ${HOME}/res2
+rsync small21:/tmp/UPDATE_$logfile.2.hdr ${HOME}/res2
+
+cp /tmp/QUERY_$logfile.0.hdr ${HOME}/res
+cp /tmp/UPDATE_$logfile.0.hdr ${HOME}/res
+cp ${HOME}/res1/QUERY_$logfile.1.hdr ${HOME}/res
+cp ${HOME}/res1/UPDATE_$logfile.1.hdr ${HOME}/res
+cp ${HOME}/res2/QUERY_$logfile.2.hdr ${HOME}/res
+cp ${HOME}/res2/UPDATE_$logfile.2.hdr ${HOME}/res
+
+docker run -ti --rm --name parse -e PREFIX=$logfile -e THREADS=$1 -v ${HOME}/res:/ycsb ycsb:parse
+
+$eval_dir/scripts/parse.py ${HOME}/res/$logfile.txt ${HOME}/res/$logfile.out
+
+curl -u $NUAGE_LIP6_U:$NUAGE_LIP6_P -T ${HOME}/res/$logfile.out https://nuage.lip6.fr/remote.php/dav/files/$NUAGE_LIP6_U/proteus_bench_logs/$logfile.txt
+curl -u $NUAGE_LIP6_U:$NUAGE_LIP6_P -T ${HOME}/res/$logfile.txt https://nuage.lip6.fr/remote.php/dav/files/$NUAGE_LIP6_U/proteus_bench_logs/$logfile.raw.txt
 
 docker stack rm $benchStack || true
 docker stack rm $queryengineStack || true
 docker stack rm $storageStack || true
 
+sleep 10
 done
